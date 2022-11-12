cc42c672397f6edecc3089745b54ef3e7f347dec
Wladimir Kaminski <98158877+RTQL@users.noreply.github.com>
2022-11-12 20:11:09 +0400

    Time: 123 ms (69.51%), Space: 15.1 MB (54.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-            for number, n in enumerate(nums):
-                if target - n in nums[number + 1:]:
-                    return [number, nums[number + 1:].index(target - n) + number + 1]
+        hashtable = {}
+        for index, number in enumerate(nums):
+            if target - number in hashtable:
+                return [hashtable[target - number], index]
+            hashtable[number] = index
             
